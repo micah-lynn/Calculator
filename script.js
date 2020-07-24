717f89b0d454ac31e34a8cbd702859b98fc45970
Micah Lynn <micah@sabado.com>
2020-07-24 16:31:05 -0400

    Calculator working on 'click' + added css styling

--- script.js
+++ script.js
@@ -1,27 +1,100 @@
-function add(a, b) {
-    return a + b;
-}
+let displayValElement = document.querySelector('.display-number');
 
-function subtract(a, b) {
-    return a - b;
-}
+let displayVal = '0'
+let pendingVal;
+let evalStringArray = [];
 
-function multiply(a, b) {
-    return a * b;
-}
+const numBtns = document.getElementsByClassName('number');
+const operatorBtns = document.getElementsByClassName('op-btn-element');
+const clearBtn = document.querySelector('.clr-btn');
+const backspaceBtn = document.querySelector('.backspace');
+const decimalBtn = document.querySelector('.decimal');
 
-function divide(a, b) {
-    return a / b;
-}
+let performOperation = (clickObj) => {
+    let operator = clickObj.target.firstChild.textContent;
+    
+    switch (operator) {
+        case '+':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('+');
+            break;
+    
+        case '-':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('-');
+            break;
+        
+        case 'x':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('*');
+            break;
 
-function operate(operator, a, b) {
-    if (operator == '+') {
-        return add(a, b);
-    } else if (operator == '-') {
-        return subtract(a, b);
-    } else if (operator == '*') {
-        return multiply(a, b);
-    } else if (operator == '/') {
-        return divide(a, b);
+        case '÷':
+            pendingVal = displayVal;
+            displayVal = '0';
+            displayValElement.innerText = displayVal;
+            evalStringArray.push(pendingVal);
+            evalStringArray.push('/');
+            break;
+
+        case '=':
+            evalStringArray.push(displayVal);
+            let evaluation = eval(evalStringArray.join(' '));
+            displayVal = evaluation + '';
+            displayValElement.innerText = displayVal;
+            evalStringArray = [];
+            break;
+        default:
+            break;
     }
+}
+
+for (let i = 0; i < numBtns.length; i++) {
+    numBtns[i].addEventListener('click', function() {
+        let btnText = numBtns[i].innerText;
+        
+        if (displayVal === '0') {
+            displayVal = '';
+        }
+
+        displayVal += btnText;
+        displayValElement.innerText = displayVal
+    });
+}
+
+for (let i = 0; i < operatorBtns.length; i++) {
+    operatorBtns[i].addEventListener('click', performOperation, false);
+}
+
+clearBtn.onclick = () => {
+    displayVal = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
+    displayValElement.innerHTML = displayVal;
+}
+
+backspaceBtn.onclick = () => {
+    let lengthOfDisplayVal = displayVal.length;
+    displayVal = displayVal.slice(0, lengthOfDisplayVal - 1);
+
+    if (displayVal === ''||displayVal === '-')
+        displayVal = '0';
+
+    displayValElement.innerHTML = displayVal;
+}
+
+decimalBtn.onclick = () => {
+    if (!displayVal.includes('.')) {
+        displayVal += '.';
+    }
+    displayValElement.innerHTML = displayVal;
 }
