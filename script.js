b8d4a24700297f30f530231bfb3496002e7e9ef2
Micah Lynn <micah@sabado.com>
2020-07-24 18:17:05 -0400

    Adjusted HTML, CSS, and added Javascript functionality for keyboard inputs

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let displayVal = '0'
 let pendingVal;
 let evalStringArray = [];
 
+//Run calculator by HTML buttons
 const numBtns = document.getElementsByClassName('number');
 const operatorBtns = document.getElementsByClassName('op-btn-element');
 const clearBtn = document.querySelector('.clr-btn');
@@ -98,3 +99,82 @@ decimalBtn.onclick = () => {
     }
     displayValElement.innerHTML = displayVal;
 }
+
+//Run calulator by Keydown action
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+        let numPress = e.key;
+
+        if (displayVal === '0') {
+            displayVal = '';
+        }
+        displayVal += numPress;
+        displayValElement.innerText = displayVal;
+    }else if (e.keyCode === 107 || e.keyCode === 109 || e.keyCode === 106 || e.keyCode === 111 || e.keyCode === 187 || e.keyCode === 13) {
+        let operatorPress = e.key;
+
+            switch (operatorPress) {
+                case '+':
+                    pendingVal = displayVal;
+                    displayVal = '0';
+                    displayValElement.innerText = displayVal;
+                    evalStringArray.push(pendingVal);
+                    evalStringArray.push('+');
+                    break;
+            
+                case '-':
+                    pendingVal = displayVal;
+                    displayVal = '0';
+                    displayValElement.innerText = displayVal;
+                    evalStringArray.push(pendingVal);
+                    evalStringArray.push('-');
+                    break;
+                
+                case '*':
+                    pendingVal = displayVal;
+                    displayVal = '0';
+                    displayValElement.innerText = displayVal;
+                    evalStringArray.push(pendingVal);
+                    evalStringArray.push('*');
+                    break;
+        
+                case '/':
+                    pendingVal = displayVal;
+                    displayVal = '0';
+                    displayValElement.innerText = displayVal;
+                    evalStringArray.push(pendingVal);
+                    evalStringArray.push('/');
+                    break;
+        
+                case 'Enter':
+                    evalStringArray.push(displayVal);
+                    let evaluation = eval(evalStringArray.join(' '));
+                    displayVal = evaluation + '';
+                    displayValElement.innerText = displayVal;
+                    evalStringArray = [];
+                    break;
+                default:
+                    break;
+        }   
+    }else if (e.keyCode === 27 || e.keyCode == 46) {
+        displayVal = '0';
+        pendingVal = undefined;
+        evalStringArray = [];
+        displayValElement.innerHTML = displayVal;
+    }else if (e.keyCode === 8) {
+        let lengthOfDisplayVal = displayVal.length;
+        displayVal = displayVal.slice(0, lengthOfDisplayVal - 1);
+
+        if (displayVal === ''||displayVal === '-')
+        displayVal = '0';
+
+        displayValElement.innerHTML = displayVal;
+    }else if (e.keyCode === 190 || e.keyCode === 110) {
+        if (!displayVal.includes('.')) {
+            displayVal += '.';
+        }
+        displayValElement.innerHTML = displayVal;
+    }else{
+        return;
+    }
+});
